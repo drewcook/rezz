db47418baf038cc1e14779c6c8ea03c5bf3c64ad
Drew Cook <drew@dco.dev>
2024-12-05 15:53:24 -0700

    feat: scope .env and config validation to each microservice app

--- apps/auth/src/main.ts
+++ apps/auth/src/main.ts
@@ -2,13 +2,17 @@ import { NestFactory } from '@nestjs/core';
 import { AuthModule } from './auth.module';
 import { ValidationPipe } from '@nestjs/common';
 import { Logger } from 'nestjs-pino';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AuthModule);
+  // Config
+  const configService = app.get(ConfigService);
   // Validation
   app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
   // Pino logger
   app.useLogger(app.get(Logger));
-  await app.listen(3001);
+  // Listen on specificed port
+  await app.listen(configService.get('PORT'));
 }
 bootstrap();
