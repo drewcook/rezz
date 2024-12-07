8ae220ceeadc51ec88f3bb287f728ae022848df5
Drew Cook <drew@dco.dev>
2024-12-06 17:50:35 -0700

    feat: add in new payments app as microservice

---
+++ apps/payments/src/main.ts
@@ -0,0 +1,21 @@
+import { NestFactory } from '@nestjs/core';
+import { PaymentsModule } from './payments.module';
+import { Transport } from '@nestjs/microservices';
+import { ConfigService } from '@nestjs/config';
+import { Logger } from 'nestjs-pino';
+
+// Start up a microservice that accepts all incoming TCP traffic on the specified port
+async function bootstrap() {
+  const app = await NestFactory.create(PaymentsModule);
+  const configService = app.get(ConfigService);
+  app.connectMicroservice({
+    transport: Transport.TCP,
+    options: {
+      host: '0.0.0.0',
+      port: configService.get('PORT'),
+    },
+  });
+  app.useLogger(app.get(Logger));
+  await app.startAllMicroservices();
+}
+bootstrap();
