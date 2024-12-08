5955527abc539b1754440366e230497b4e9b36b3
Drew Cook <drew@dco.dev>
2024-12-08 15:14:44 -0700

    feat: add roles and guard delete route

--- libs/common/src/auth/jwt-auth.guard.ts
+++ libs/common/src/auth/jwt-auth.guard.ts
@@ -3,11 +3,14 @@ import {
   ExecutionContext,
   Inject,
   Injectable,
+  Logger,
+  UnauthorizedException,
 } from '@nestjs/common';
 import { catchError, map, Observable, of, tap } from 'rxjs';
 import { AUTH_SERVICE } from '../constants/services.constants';
 import { ClientProxy } from '@nestjs/microservices';
 import { UserDto } from '../dto';
+import { Reflector } from '@nestjs/core';
 
 /**
  * A common auth guard that can be used across any other microservice. It will be used to authenticate for all HTTP requests the need it outside of the Auth microservice.
@@ -15,29 +18,54 @@ import { UserDto } from '../dto';
  */
 @Injectable()
 export class JwtAuthGuard implements CanActivate {
-  constructor(@Inject(AUTH_SERVICE) private readonly authClient: ClientProxy) {}
+  private readonly logger = new Logger(JwtAuthGuard.name);
+
+  constructor(
+    @Inject(AUTH_SERVICE) private readonly authClient: ClientProxy,
+    private readonly reflector: Reflector,
+  ) {}
 
   /**
    * Check first if there's a JWT attached to the HTTP request (should have been from 'auth/login'). If so, send a message to the auth service that matches the authentication pattern and data shape.
+   * Check for roles from the custom @Roles decorator and validate.
    * @returns An observable. The authenticate method will return the authenticated user as a response; attach the response as the 'user' field on the request object, return true for activation status.
    */
   canActivate(
     context: ExecutionContext,
   ): boolean | Promise<boolean> | Observable<boolean> {
+    // Check for the JWT
     const jwt =
       context.switchToHttp().getRequest().cookies?.Authentication ||
       context.switchToHttp().getRequest().headers?.authentication;
     if (!jwt) return false;
+
+    // Check for roles - handler is the controller route being called
+    const roles = this.reflector.get<string[]>('roles', context.getHandler());
+
     return this.authClient
       .send<UserDto>('authenticate', {
         Authentication: jwt,
       })
       .pipe(
         tap((res) => {
+          // Validate roles
+          if (roles) {
+            for (const role of roles) {
+              if (!res.roles?.includes(role)) {
+                this.logger.error('The user does not have valid roles.');
+                throw new UnauthorizedException();
+              }
+            }
+          }
+
+          // Set user field on the request object
           context.switchToHttp().getRequest().user = res;
         }),
         map(() => true),
-        catchError(() => of(false)),
+        catchError((err) => {
+          this.logger.error(err);
+          return of(false);
+        }),
       );
   }
 }
